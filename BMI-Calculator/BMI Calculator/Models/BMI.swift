9dc078f8c07874141961e9cd442ea8214889c226
shaunkani007 <53708711+shaunkani007@users.noreply.github.com>
2019-11-11 15:45:57 +0530

    Add files via upload

---
+++ BMI-Calculator/BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Prithvish Shaunkani on 29/08/2019.
+//  Copyright © 2019 Prithvish Shaunkani. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
